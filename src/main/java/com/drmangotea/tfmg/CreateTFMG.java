38ba20305ebf45f4cb7db24a426cd74b655c53ec
DrMangoTea <adam.nemec.07@seznam.cz>
2023-08-28 09:52:33 +0200

    gradle does not combust when starting the game

---
+++ src/main/java/com/drmangotea/tfmg/CreateTFMG.java
@@ -0,0 +1,37 @@
+package com.drmangotea.tfmg;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(CreateTFMG.MOD_ID)
+public class CreateTFMG
+{
+
+    public static final String MOD_ID = "tfmg";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateTFMG()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        //
+
+
+        //
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("YEEEHAAW");
+    }
+
+}
