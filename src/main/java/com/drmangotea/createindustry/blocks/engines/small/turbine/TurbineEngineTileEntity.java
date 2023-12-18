6128e6195953dbb0e676baedda27e09b3bf6e6c2
lonevox <quinn.epicness@gmail.com>
2023-12-18 20:48:05 +1300

    Simplify AbstractEngineTileEntities
    hasBackPart() didn't need to be entirely redefined in inherited classes. I added the backPartBlock() method instead that is now overridden on inherited classes. I made backPartBlock() return TFMGBlocks.GASOLINE_ENGINE_BACK by default so the functionality is the same as before.

--- src/main/java/com/drmangotea/createindustry/blocks/engines/small/turbine/TurbineEngineTileEntity.java
+++ src/main/java/com/drmangotea/createindustry/blocks/engines/small/turbine/TurbineEngineTileEntity.java
@@ -2,14 +2,11 @@ package com.drmangotea.createindustry.blocks.engines.small.turbine;
 
 
 import com.drmangotea.createindustry.blocks.engines.small.AbstractEngineTileEntity;
+import com.drmangotea.createindustry.blocks.engines.small.EngineBackPartBlock;
 import com.drmangotea.createindustry.registry.TFMGBlocks;
-import com.drmangotea.createindustry.registry.TFMGFluids;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Fluid;
 
 public class TurbineEngineTileEntity extends AbstractEngineTileEntity {
 
@@ -20,36 +17,7 @@ public class TurbineEngineTileEntity extends AbstractEngineTileEntity {
     }
 
     @Override
-    public boolean hasBackPart(){
-        BlockPos wantedLocation=this.getBlockPos();
-        Direction direction = this.getBlockState().getValue(DirectionalBlock.FACING);
-
-
-        if(direction == Direction.UP)
-            wantedLocation=this.getBlockPos().below();
-        if(direction == Direction.DOWN)
-            wantedLocation=this.getBlockPos().above();
-        if(direction == Direction.NORTH)
-            wantedLocation=this.getBlockPos().south();
-        if(direction == Direction.SOUTH)
-            wantedLocation=this.getBlockPos().north();
-        if(direction == Direction.WEST)
-            wantedLocation=this.getBlockPos().east();
-        if(direction == Direction.EAST)
-            wantedLocation=this.getBlockPos().west();
-
-
-
-        if(!level.getBlockState(wantedLocation).is(TFMGBlocks.TURBINE_ENGINE_BACK.get())) {
-            return false;
-        }else {
-            if ( level.getBlockState(wantedLocation).getValue(DirectionalBlock.FACING) != direction)
-                return false;
-        }
-        return true;
-    }
-    @Override
-    public Fluid validFuel() {
-        return TFMGFluids.KEROSENE.getSource();
+    public EngineBackPartBlock backPartBlock() {
+        return TFMGBlocks.TURBINE_ENGINE_BACK.get();
     }
 }
