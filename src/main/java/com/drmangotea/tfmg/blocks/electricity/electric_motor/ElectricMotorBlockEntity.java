6ace7ec61e4b05fedb054516a485eb1c537d58b1
DrMangoTea <adam.nemec.07@seznam.cz>
2024-08-26 22:56:52 +0200

    0.9.1 - 1.20.1

--- src/main/java/com/drmangotea/tfmg/blocks/electricity/electric_motor/ElectricMotorBlockEntity.java
+++ src/main/java/com/drmangotea/tfmg/blocks/electricity/electric_motor/ElectricMotorBlockEntity.java
@@ -1,14 +1,13 @@
 package com.drmangotea.tfmg.blocks.electricity.electric_motor;
 
 
-import com.drmangotea.tfmg.base.TFMGUtils;
-import com.drmangotea.tfmg.blocks.electricity.base.IElectricBlock;
-import com.drmangotea.tfmg.blocks.electricity.base.TFMGForgeEnergyStorage;
+import com.drmangotea.tfmg.base.MaxBlockVoltage;
+import com.drmangotea.tfmg.blocks.electricity.base.KineticEnergyBlockEntity;
+import com.drmangotea.tfmg.registry.TFMGBlockEntities;
 import com.drmangotea.tfmg.registry.TFMGBlocks;
 import com.jozufozu.flywheel.util.transform.TransformStack;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.content.equipment.goggles.IHaveGoggleInformation;
-import com.simibubi.create.content.kinetics.base.GeneratingKineticBlockEntity;
 import com.simibubi.create.content.kinetics.motor.KineticScrollValueBehaviour;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueBoxTransform;
@@ -18,46 +17,27 @@ import com.simibubi.create.foundation.utility.Lang;
 import com.simibubi.create.foundation.utility.VecHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.energy.IEnergyStorage;
-import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 import static com.simibubi.create.content.kinetics.base.DirectionalKineticBlock.FACING;
 
-public class ElectricMotorBlockEntity extends GeneratingKineticBlockEntity implements IElectricBlock, IHaveGoggleInformation {
+public class ElectricMotorBlockEntity extends KineticEnergyBlockEntity implements IHaveGoggleInformation {
 
     public static final int DEFAULT_SPEED = 16;
     public static final int MAX_SPEED = 256;
 
 
-    public int voltage=0;
-
-    private int voltageLastTick = voltage;
-    public int current =0;
-
-    private int energyUsed=0;
-
-    private boolean gotFElastTick = false;
-
-    public int distanceFromSource = Integer.MAX_VALUE;
-
     protected ScrollValueBehaviour generatedSpeed;
 
-    private LazyOptional<IEnergyStorage> lazyEnergyHandler = LazyOptional.empty();
 
-    public final TFMGForgeEnergyStorage energy = createEnergyStorage();
+
     public ElectricMotorBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+        setLazyTickRate(10);
     }
 
 
@@ -72,37 +52,58 @@ public class ElectricMotorBlockEntity extends GeneratingKineticBlockEntity imple
         generatedSpeed.between(-max, max);
         generatedSpeed.value = DEFAULT_SPEED;
         generatedSpeed.withCallback(i -> this.updateGeneratedRotation());
+        generatedSpeed.withCallback(i ->this.needsNetworkUpdate());
         behaviours.add(generatedSpeed);
     }
 
+
+
     @Override
-    public void initialize() {
-        super.initialize();
-        if (!hasSource() || getGeneratedSpeed() > getTheoreticalSpeed())
+    public void lazyTick() {
+        super.lazyTick();
+
+
+        if(energy.getEnergyStored()>0&&getVoltage()>0) {
             updateGeneratedRotation();
+        }else sendData();
+    }
+
+
+    @Override
+    public void tick() {
+        super.tick();
+        getOrCreateElectricNetwork().requestEnergy(this);
+
+
     }
 
     @Override
+    public void setVoltage(int value, boolean update) {
+        super.setVoltage(value, update);
+        updateGeneratedRotation();
+    }
+
+
+
+    @Override
     public float getGeneratedSpeed() {
+
+        if(voltage == 0)
+            return 0;
+
+
         if (!TFMGBlocks.ELECTRIC_MOTOR.has(getBlockState()))
             return 0;
 
-        if(energy.getEnergyStored()<energy.getMaxEnergyStored()*0.25)
-            return 0;
 
 
-        energyUsed = energy.extractEnergy((int) Math.abs(speed*1.7),false);
+        energy.extractEnergy((int) Math.abs(speed/4),false);
 
 
-        //if(energyUsed<(int) Math.abs(speed*2))
-        //    return 0;
+
 
         if(energy.getEnergyStored() == 0)
             return 0;
-
-        //if(energyUsed<(int) Math.abs(speed*2))
-        //    return 0;
-
 
 
 
@@ -110,32 +111,11 @@ public class ElectricMotorBlockEntity extends GeneratingKineticBlockEntity imple
         return convertToDirection(Math.min(generatedSpeed.getValue(),voltage*2), getBlockState().getValue(FACING));
     }
 
-    public void tick(){
-        super.tick();
-
-
-
-
-        manageVoltage();
-        sendCharge(level,getBlockPos());
-
-
-
-
-
-      //  if(speed>0){
-
-
-
-        //}
-
-
-
-
-      if(energy.getEnergyStored()>0) {
-          updateGeneratedRotation();
-      }else sendData();
+    @Override
+    public boolean outputAllowed() {
+        return false;
     }
+
 
 
 
@@ -182,222 +162,19 @@ public class ElectricMotorBlockEntity extends GeneratingKineticBlockEntity imple
     }
 
 
-
-
-
-
-
-
-    public TFMGForgeEnergyStorage getEnergy() {
-        return energy;
-    }
-
     @Override
-    public void onLoad() {
-        super.onLoad();
-        lazyEnergyHandler = LazyOptional.of(() -> energy);
+    public int maxVoltage() {
+        return MaxBlockVoltage.MAX_VOLTAGES.get(TFMGBlockEntities.ELECTRIC_MOTOR.get());
     }
 
-    @Override
-    public void invalidateCaps() {
-        super.invalidateCaps();
-        lazyEnergyHandler.invalidate();
-    }
-
-    @Override
-    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
-
-
-        if (cap == ForgeCapabilities.ENERGY&&side == null) {
-            return lazyEnergyHandler.cast();
-        } else
-
-        if (cap == ForgeCapabilities.ENERGY&&hasElectricitySlot(side)) {
-            return lazyEnergyHandler.cast();
-        }
-
-        return super.getCapability(cap, side);
-    }
-
-
-    public void manageVoltage(){
-
-
-
-        if(voltageLastTick!=voltage)
-            updateGeneratedRotation();
-
-        if(voltageGeneration()>0) {
-            voltage = voltageGeneration();
-            distanceFromSource = 0;
-        }
-        if(distanceFromSource == Integer.MAX_VALUE)
-            voltage = 0;
-
-
-        current = energy.getEnergyStored()/(voltage+1);
-
-
-
-        if(voltage>maxVoltage()){
-            explode();
-            voltage = 0;
-        }
-
-        voltageLastTick = voltage;
-
-    }
-
-
-
-
-
-    @Override
-    public boolean addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
-
-
-
-         super.addToGoggleTooltip(tooltip, isPlayerSneaking);
-
-
-        //Lang.translate("goggles.motor.usage",      energy.extractEnergy((int) Math.abs(speed*1.7),false),true)
-        //        .style(ChatFormatting.AQUA)
-        //        .forGoggles(tooltip, 1);
-//
-
-        return true;
-    }
-
-
-
-
-
-    @Override
-    public float internalResistance() {
-        return 0;
-    }
-
-    @Override
-    public int getVoltage() {
-        return voltage;
-    }
-
-    @Override
-    public boolean gotFElastTick(int value) {
-
-
-
-        gotFElastTick = value == 3 ? gotFElastTick : value == 1;
-
-        return gotFElastTick;
-    }
-
-    @Override
-    public int getCurrent() {
-        return current;
-    }
-
-
-
-
-
-    @Override
-    public void addVoltage(float amount) {
-
-        if(voltageGeneration()>0)
-            return;
-
-        voltage = (int) amount;
-
-    }
-
-
-
-
-    @Override
-    public TFMGForgeEnergyStorage getForgeEnergy() {
-        return energy;
-    }
-
-
-
-
-
-
-
-
-    @Override
-    public float maxVoltage() {
-        return 1000;
-    }
-
-    @Override
-    public void explode() {
-        TFMGUtils.createFireExplosion(level,null, getBlockPos(),10,1.5f);
-    }
-
-    @Override
-    public int FECapacity() {
-        return 5000;
-    }
-
-    @Override
-    public int getDistanceFromSource() {
-        return distanceFromSource;
-    }
 
 
 
     @Override
-    public void setDistanceFromSource(int value) {
-        distanceFromSource = value;
-        sendStuff();
-    }
-
-    @Override
-    public void sendStuff() {
-        sendData();
-        setChanged();
-    }
-
-
-    @Override
-    public void write(CompoundTag compound, boolean clientPacket) {
-        super.write(compound, clientPacket);
-
-        writeElectrity(compound);
-
-        ;
-    }
-
-    @Override
-    protected void read(CompoundTag compound, boolean clientPacket) {
-        super.read(compound, clientPacket);
-        voltage = compound.getInt("Voltage");
-        current = compound.getInt("Current");
-
-        distanceFromSource = compound.getInt("DistanceFromSource");
-
-
-
-        energy.setEnergy(compound.getInt("ForgeEnergy"));
-
+    public int FECapacity() {
+        return 10000;
     }
 
 
 
-    @Override
-    public int feGeneration() {
-        return 0;
-    }
-
-    @Override
-    public int voltageGeneration() {
-        return 0;
-    }
-
-    @Override
-    public int transferSpeed() {
-        return 1000;
-    }
 }
