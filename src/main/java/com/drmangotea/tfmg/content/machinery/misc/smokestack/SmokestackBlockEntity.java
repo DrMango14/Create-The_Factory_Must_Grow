63a08b8b4a9d47f49887507e583d48da2e23090c
Daniel <daniel36611b@gmail.com>
2025-07-09 23:38:07 +0300

    Fix exhaust and flarestack behavior - Add parity with smokestack - Dynamic drain rate & particle spawn - Slightly tweak throughput

--- src/main/java/com/drmangotea/tfmg/content/machinery/misc/smokestack/SmokestackBlockEntity.java
+++ src/main/java/com/drmangotea/tfmg/content/machinery/misc/smokestack/SmokestackBlockEntity.java
@@ -28,25 +28,25 @@ import static com.drmangotea.tfmg.content.machinery.misc.smokestack.SmokestackBl
 
 public class SmokestackBlockEntity extends SmartBlockEntity {
 
+    protected LazyOptional<IFluidHandler> fluidCapability;
+    public FluidTank tankInventory;
 
     int smokeTimer = 0;
 
 
-    public FluidTank tankInventory;
-
-    protected LazyOptional<IFluidHandler> fluidCapability;
-
     public SmokestackBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+        tankInventory = createInventory();
+        fluidCapability = LazyOptional.of(() -> tankInventory);
+    }
 
-        tankInventory = new SmartFluidTank(8000, this::onFluidStackChanged) {
+    protected SmartFluidTank createInventory() {
+        return new SmartFluidTank(1000, this::onFluidStackChanged) {
             @Override
             public boolean isFluidValid(FluidStack stack) {
                 return stack.getFluid().isSame(TFMGFluids.CARBON_DIOXIDE.getSource());
             }
         };
-
-        fluidCapability = LazyOptional.of(() -> tankInventory);
     }
 
     @Nonnull
@@ -62,89 +62,68 @@ public class SmokestackBlockEntity extends SmartBlockEntity {
     @Override
     public void invalidate() {
         super.invalidate();
-
         fluidCapability.invalidate();
-    }
-
-    @Override
-    protected void read(CompoundTag compound, boolean clientPacket) {
-        super.read(compound, clientPacket);
-
-        tankInventory.readFromNBT(compound.getCompound("TankContent"));
-
-
     }
 
     protected void onFluidStackChanged(FluidStack newFluidStack) {
         if (!hasLevel())
             return;
-
         setChanged();
         sendData();
-
-
-    }
-
-    public static void makeParticles(Level level, BlockPos pos) {
-        Random random = Create.RANDOM;
-        int shouldSpawnSmoke = random.nextInt(7);
-        if (shouldSpawnSmoke == 0) {
-
-            level.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, pos.getX() + random.nextFloat(1), pos.getY() + 1, pos.getZ() + random.nextFloat(1), 0.0D, 0.08D, 0.0D);
-
-        }
-
     }
 
     @Override
     public void tick() {
         super.tick();
 
-        if (smokeTimer > 0) {
+        if (tankInventory.isEmpty()) return;
 
-            makeParticles(level, getBlockPos());
+        float fillRatio = (float) tankInventory.getFluidAmount() / tankInventory.getCapacity();
 
-            smokeTimer--;
-        }
-
-
-        if (tankInventory.isEmpty())
-            return;
-
+        // Dynamic drain rate: 10mB/t (empty) → 100mB/t (full)
+        int drainRate = 10 + (int) (90 * fillRatio);
 
         if (getBlockState().getValue(TOP)) {
-            tankInventory.drain(tankInventory.getSpace() < 1000 ? 50 : 10, IFluidHandler.FluidAction.EXECUTE);
-
+            tankInventory.drain(drainRate, IFluidHandler.FluidAction.EXECUTE);
             smokeTimer = 40;
-
         }
 
+        if (smokeTimer > 0) {
+            smokeTimer--;
+            // Spawn particles every 1-10 ticks
+            if ((level.getGameTime() + getBlockPos().hashCode()) % Math.max(1, 10 - (int) (9 * fillRatio)) == 0) {
+                makeParticles(level, getBlockPos());
+            }
+        }
 
+        //Transfer smoke upwards
         if (level.getBlockEntity(getBlockPos().above()) instanceof SmokestackBlockEntity be) {
-
             int transferAmount = Math.min(tankInventory.getFluidAmount(), be.tankInventory.getCapacity() - be.tankInventory.getFluidAmount());
 
             tankInventory.drain(transferAmount, IFluidHandler.FluidAction.EXECUTE);
             be.tankInventory.fill(new FluidStack(TFMGFluids.CARBON_DIOXIDE.getSource(), transferAmount), IFluidHandler.FluidAction.EXECUTE);
-
         }
+    }
+
+    public static void makeParticles(Level level, BlockPos pos) {
+        Random random = Create.RANDOM;
+
+        level.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, pos.getX() + random.nextFloat(1), pos.getY() + 1, pos.getZ() + random.nextFloat(1), 0.0D, 0.08D, 0.0D);
+    }
+
+    @Override
+    protected void read(CompoundTag compound, boolean clientPacket) {
+        super.read(compound, clientPacket);
+        tankInventory.readFromNBT(compound.getCompound("TankContent"));
     }
 
     @Override
     public void write(CompoundTag compound, boolean clientPacket) {
         super.write(compound, clientPacket);
-
-
         compound.put("TankContent", tankInventory.writeToNBT(new CompoundTag()));
-
-
         compound.putBoolean("Active", smokeTimer > 0);
-
-
     }
-
 
     @Override
-    public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
-    }
+    public void addBehaviours(List<BlockEntityBehaviour> behaviours) {}
 }
